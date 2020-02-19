fb773942d3fc66150af5fab05a85214dab7e37b6
Borman666 <Borman010884@yandex.ru>
2020-02-19 21:44:29 +0300

    Тест_2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,12 +1,19 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-public class MainClassTest extends MainClass{
+public class MainClassTest extends MainClass {
 
     @Test
     public void testGetLocalNumber() {
         int number = 14;
         Assert.assertTrue("Не верное число - " +getLocalNumber()+ ", должно быть - "+ number,
                 getLocalNumber() == number);
+    }
+
+    @Test
+    public void testGetClassNumber() {
+        int number = 45;
+        Assert.assertTrue("Не верное число - " +getClassNumber()+ ", должно быть больше "+ number,
+                getClassNumber() > number);
     }
 }
