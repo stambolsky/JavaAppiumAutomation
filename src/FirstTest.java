a2c725bd475c32893345a339819bad970c113a30
Borman666 <Borman010884@yandex.ru>
2020-03-05 12:51:59 +0300

    Lesson 2. Ex3: Тест: отмена поиска

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.util.List;
 
 public class FirstTest {
 
@@ -30,6 +31,11 @@ public class FirstTest {
         capabilities.setCapability("app", "/Users/sergejtambolskij/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Skip')]"),
+                "Cannot find search input",
+                5);
     }
 
     @After
@@ -39,53 +45,98 @@ public class FirstTest {
 
     @Test
     public void firstTest() throws InterruptedException {
-        waitForElementByXpathAndClick(
-                "//*[contains(@text,'Skip')]",
-                "Cannot find search input",
-                5);
-        waitForElementByXpathAndClick(
-                "//*[contains(@text,'Search Wikipedia')]",
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
                 "Cannot find Search Wikipedia input",
                 5);
 
-        waitForElementByXpathAndCheckText(
-                "//*[@resource-id='org.wikipedia:id/search_src_text']",
+        waitForElementAndCheckText(
+                By.xpath("//*[@resource-id='org.wikipedia:id/search_src_text']"),
                 "Search Wikipedia",
                 "Cannot find Search Wikipedia input",
                 5);
-
-
-        //element_to_enter_search_line.sendKeys("Java");
-
     }
 
-    private WebElement waitForElementPresentByXpath(String xpath, String error_message, long timeOutInSecond) {
+    @Test
+    public void testCancelSearch() {
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find Search Wikipedia input",
+                5);
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[@resource-id='org.wikipedia:id/search_src_text']"),
+                "Java",
+                "Cannot find search input",
+                5
+        );
+
+        waitForElementPresent(By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_image']"));
+        List<WebElement> listArticle = driver.findElements(By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_image']"));
+        Assert.assertTrue("Нет статей на странице", listArticle.size()>0);
+
+        waitForAndClear(
+                By.xpath("//*[@resource-id='org.wikipedia:id/search_src_text']"),
+                "Cannot find search field",
+                5
+        );
+
+        listArticle = driver.findElements(By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_image']"));
+        Assert.assertTrue("Список статей не пустой", listArticle.size()==0);
+
+       /* waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/search_close_btn']"),
+                "Cannot find X to cancel search",
+                15);
+
+        waitForElementNotPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/search_close_btn']"),
+                "X is still present on the page",
+                5);*/
+    }
+
+    private WebElement waitForElementPresent(By by, String error_message, long timeOutInSecond) {
         WebDriverWait wait = new WebDriverWait(driver, timeOutInSecond);
         wait.withMessage(error_message + "\n");
-        By by = By.xpath(xpath);
         return wait.until(
                 ExpectedConditions.presenceOfElementLocated(by)
         );
     }
 
-    private WebElement waitForElementPresentByXpath(String xpath) {
-        return waitForElementPresentByXpath(xpath, "Cannot find search input", 5);
+    private WebElement waitForElementPresent(By by) {
+        return waitForElementPresent(by, "Cannot find search input", 5);
     }
 
-    private WebElement waitForElementByXpathAndClick(String xpath, String error_message, long timeOutInSecond) {
-        WebElement element = waitForElementPresentByXpath(xpath, error_message, timeOutInSecond);
+    private WebElement waitForElementAndClick(By by, String error_message, long timeOutInSecond) {
+        WebElement element = waitForElementPresent(by, error_message, timeOutInSecond);
         element.click();
         return element;
     }
 
-    private WebElement waitForElementByXpathAndSendKeys(String xpath, String value, String error_message, long timeOutInSecond) {
-        WebElement element = waitForElementPresentByXpath(xpath, error_message, timeOutInSecond);
+    private WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeOutInSecond) {
+        WebElement element = waitForElementPresent(by, error_message, timeOutInSecond);
         element.sendKeys(value);
         return element;
     }
 
-    private void waitForElementByXpathAndCheckText(String xpath, String value, String error_message, long timeOutInSecond) {
-        WebElement element = waitForElementPresentByXpath(xpath, error_message, timeOutInSecond);
+    private void waitForElementAndCheckText(By by, String value, String error_message, long timeOutInSecond) {
+        WebElement element = waitForElementPresent(by, error_message, timeOutInSecond);
         Assert.assertEquals(element.getText(), value);
+    }
+
+    private boolean waitForElementNotPresent(By by, String error_message, long timeOutInSecond) {
+        WebDriverWait wait = new WebDriverWait(driver, timeOutInSecond);
+        wait.withMessage(error_message + "\n");
+        return wait.until(
+                ExpectedConditions.invisibilityOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForAndClear(By by, String error_message, long timeOutInSecond) {
+        WebElement element = waitForElementPresent(by, error_message, timeOutInSecond);
+        element.clear();
+        return element;
     }
 }
